99e6235a983272ca751df887744e72f839bdfb2a
nueng.jp <jintana.ph@tbn.co.th>
2025-05-22 18:12:25 +0700

    update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
-import { createTheme } from "@mui/material/styles";
-import { red, grey } from "@mui/material/colors";
+import { createTheme } from '@mui/material/styles';
+import { red, grey } from '@mui/material/colors';
 
-declare module "@mui/material/styles" {
+declare module '@mui/material/styles' {
   interface Theme {
     customBackground: {
       gradient: string;
@@ -18,21 +18,21 @@ declare module "@mui/material/styles" {
 export const getAppTheme = (darkMode: boolean) =>
   createTheme({
     palette: {
-      mode: darkMode ? "dark" : "light",
+      mode: darkMode ? 'dark' : 'light',
       primary: {
-        main: darkMode ? "#ec4899" : red[700]
+        main: darkMode ? '#ec4899' : red[700],
       },
       background: {
-        default: darkMode ? "#121212" : "#fff",
-        paper: darkMode ? grey[900] : "#fff"
-      }
+        default: darkMode ? '#121212' : '#fff',
+        paper: darkMode ? grey[900] : '#fff',
+      },
     },
     typography: {
-      fontFamily: `'Lato'`
+      fontFamily: `'Lato'`,
     },
     customBackground: {
       gradient: darkMode
-        ? "linear-gradient(to right, rgba(18,18,18,0.95), rgba(55,0,60,0.95), rgba(0,0,0,0.95))"
-        : "linear-gradient(to right, rgba(227,253,245,0.9), rgba(255,230,250,0.9), rgba(255,255,255,0.9))"
-    }
+        ? 'linear-gradient(to right, rgba(18,18,18,0.95), rgba(55,0,60,0.95), rgba(0,0,0,0.95))'
+        : 'linear-gradient(to right, rgba(227,253,245,0.9), rgba(255,230,250,0.9), rgba(255,255,255,0.9))',
+    },
   } as const);
